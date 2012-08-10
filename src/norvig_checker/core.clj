5310f9667d44bd0be5770d7a242baae596777dd5
Steven Merrill <steven.merrill@gmail.com>
2012-08-09 23:05:00 -0400

    Add working "correct" and "known" fns.

--- src/norvig_checker/core.clj
+++ src/norvig_checker/core.clj
@@ -1,18 +1,23 @@
 (ns norvig-checker.core)
 (use '[clojure.string :only (lower-case)])
-(defn words [x] (re-seq #"[a-z]+" (lower-case x)))
-;(def NWORDS (frequencies (words (slurp "big.txt")))) ; @TODO: Move from slurp.
+(defn words-in [x] (re-seq #"[a-z]+" (lower-case x)))
+;(def NWORDS (frequencies (words-in (slurp "big.txt")))) ; @TODO: Move from slurp.
 (def alphabet "abcdefghijklmnopqrstuvwxyz")
-(defn correct [words] (filter #(contains? NWORDS %) (list words)))
+
 (defn deletion [a word] (list (apply str (concat (take a word) (drop (inc a) word)))))
 (defn replaces [a word] (map #(apply str (concat (take a word) (list %) (drop (inc a) word))) alphabet))
 (defn transposition [a word] (list (apply str (if (< a (dec (count word))) (concat (take a word) (list (get word (inc a))) (list (get word a)) (drop (inc (inc a)) word))))))
 (defn inserts [a word] (map #(apply str (concat (take a word) (list %) (drop a word))) alphabet))
-(defn edits1 [word]
-    (remove empty? 
-      (flatten 
-        (for [a (range (count word))]
-          (list (deletion a word) (inserts a word) (replaces a word) (transposition a word))))))
 
-(edits1 "hi")
+(defn edits1 [word]
+  (remove empty? 
+    (flatten 
+      (for [a (range (count word))]
+        (list (deletion a word) (inserts a word) (replaces a word) (transposition a word))))))
+
+(defn known [words] (filter #(contains? NWORDS %) words))
+(defn correct [word]
+  (or
+    (known (list words))
+    (known (edits1 word))))
 
